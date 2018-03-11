df42f9925203dcb360f88151c369a810c6ae6c37
Luciano Mammino <lucianomammino@gmail.com>
2018-03-11 22:19:02 +0000

    Working on build stream

--- src/build.js
+++ src/build.js
@@ -1,4 +1,5 @@
 const { createWriteStream } = require('fs')
+const uglify = require('uglify-stream')
 const parseLumpyFile = require('./parseLumpyFile')
 const DependenciesAssembler = require('./dependenciesAssembler')
 
@@ -8,12 +9,18 @@ const build = async (opt) => {
   const dependencies = await parseLumpyFile(opt.lumpyFile)
   // retrieve and assemble dependencies
   const depsAssembler = new DependenciesAssembler(dependencies, { cacheFolder, noCache })
-  depsAssembler.pipe(createWriteStream(opt.destFile))
-    .on('close', () => console.log('--- CLOSING FROM BUILD'))
-  // depsAssembler.pipe(process.stdout)
+  const destStream = opt.stdout ? process.stdout : createWriteStream(opt.destFile)
 
-  // TODO minify
-  // TODO save or print to std
+  // TODO use pump for proper error management
+  depsAssembler
+    .pipe(uglify())
+    .on('error', console.error)
+    .pipe(destStream)
+    .on('close', () => console.log('--- CLOSING FROM BUILD'))
+
+  // TODO disable minify
+  // TODO uglify
+
   return Promise.resolve('TODO')
 }
 
